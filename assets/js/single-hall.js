57d0ffc915b6668850b7f16854dba852f13c279a
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-02-23 15:53:22 +0100

    update gallery func and style in single hall page

--- assets/js/single-hall.js
+++ assets/js/single-hall.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper("#hallGallery", {
+    var swiper1 = new Swiper("#hallGallery", {
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
-        autoplay: false,
-        loop: true,
+
         effect: 'fade',
         
     });
 
-    swiper.autoplay.stop();
 });
   
