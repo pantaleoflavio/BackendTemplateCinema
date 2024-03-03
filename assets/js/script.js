ab12467eb74909b1f4848e442e9d18e228cf0fb8
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-03-03 11:02:32 +0100

    add searchable class to film cards for the search function in index, implementation of the function search movie in index, temporary redirect to index if in other pages the user click on search icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Open search icon input by click on search icon in navbar
    
     function toggleSearchInput() {
+        const isIndexPage = window.location.pathname.endsWith('/index.html') || window.location.pathname === '/' || window.location.pathname.match(/\/resources\/Views\/$/);
+
         if (searchInput.style.display === 'none') {
             openSearchInput();
         } else {
@@ -32,16 +34,48 @@ document.addEventListener('DOMContentLoaded', function () {
         searchInput.classList.remove('open');
     }
 
+    // If is not index redirect to index
+    function redirectToIndex(e) {
+        e.preventDefault(e);
+        
+        // Controlla se l'utente si trova su index.html
+        const isIndexPage = window.location.pathname.endsWith('/index.html') || window.location.pathname === '/' || window.location.pathname.match(/\/resources\/Views\/$/);
+
+        if (isIndexPage) {
+            // Se sei su index.html, apri la searchBar
+            toggleSearchInput();
+        } else {
+            // Se non sei su index.html, reindirizza l'utente a index.html
+            window.location.href = "/resources/Views/index.html";
+        }
+    }
+
+    // Search Function
+    function searchFunction(e) {
+        e.preventDefault(e);
+        const searchTerm = e.target.value.toLowerCase();
+        // DOM Elements to search
+        const elementsToSearch = document.querySelectorAll('.searchable'); //class .searchable to elements searchable
+
+        elementsToSearch.forEach(function(el) {
+            if(el.textContent.toLowerCase().includes(searchTerm)) {
+                el.style.display = 'block'; 
+            } else {
+                el.style.display = 'none';
+            }
+        });
+    };
+
     // EVENT LISTENER
-    searchIcon.addEventListener('click', toggleSearchInput);
+    searchIcon.addEventListener('click', redirectToIndex);
     footerArrow.addEventListener('click', function() {
         window.scrollTo({top: 0, behavior: 'smooth'});
     });
+    searchInput.addEventListener('input', searchFunction);
 
 
-
+    
     // Hero Gallery/Swipe Js Init
-
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         direction: 'horizontal',
