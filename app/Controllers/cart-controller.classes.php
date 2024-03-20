17e22fdf00e3fc7c1651bd5026dc63965e438a15
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-03-20 14:59:46 +0100

    add model and controller of cart, with methods for create and display cart

---
+++ app/Controllers/cart-controller.classes.php
@@ -0,0 +1,48 @@
+<?php
+
+include __DIR__ . '/../Models/cart.classes.php';
+
+class CartController extends DB {
+
+    public function getCartProUser($user_id){
+        $stmt = $this->connect()->prepare("SELECT * FROM cart WHERE user_id = ?");
+
+
+        if(!$stmt->execute([$user_id])){
+            $cart = null;
+        } else {
+            
+            $cart = $stmt->fetchAll((PDO::FETCH_OBJ));
+            
+        }
+
+        return $cart;
+
+        
+    }
+
+    public function createCart( $movie_id, $hall_id, $show_id, $seat_ids, $user_id){
+        $stmt = $this->connect()->prepare("INSERT INTO cart (movie_id, hall_id, show_time_id, seat_ids, user_id) VALUES (?, ?, ?, ?, ?)");
+
+        try {
+            $stmt->bindParam(1, $movie_id);
+            $stmt->bindParam(2, $hall_id);
+            $stmt->bindParam(3, $show_id);
+            $stmt->bindParam(4, $seat_ids);
+            $stmt->bindParam(5, $user_id);
+    
+            $stmt->execute();
+    
+            return true; // Successo
+        } catch (PDOException $e) {
+            error_log("PDOException in createCart: " . $e->getMessage());
+            return false;
+        }
+        
+    }
+
+    
+}
+
+
+?>
