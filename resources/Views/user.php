bd9d3f3d08809c39428434cabfdd06865810f235
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-03-22 16:39:11 +0100

    update user style, button that redirects to user settings, display of user settings

--- resources/Views/user.php
+++ resources/Views/user.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['userId'])) {
 } else {
     $userId = $_SESSION['userId'];
     $singleUser = $userController->getSingleUser($userId);
-    $bills = $billCOntroller->getBillsProUser($userId);
+    $bookingHistory = $billCOntroller->getBillsProUser($userId);
 }
 
 
@@ -21,7 +21,7 @@ if (!isset($_SESSION['userId'])) {
     </div>
     <div class="row">
         <!-- Users data -->
-        <section id="user-info" class="col-12 col-lg-6">
+        <section id="user-info" class="col-12 col-lg-6 py-2">
             <h2>User</h2>
             <p>Full Name: <?php echo $singleUser->fullname; ?></p>
             <p>Username: <?php echo $singleUser->username; ?></p>
@@ -33,13 +33,43 @@ if (!isset($_SESSION['userId'])) {
             </div>
 
             <!-- Link per modificare il profilo -->
-            <a href="">Modifica Profilo</a>
+            <div class="mt-2">
+                <a class="btn btn-secondary text-white" style="text-decoration: none;" href="user-settings.php">Modify your Profile</a>
+            </div>
         </section>
     
-        <!-- Prenotations hronology -->
-        <section id="booking-history" class="col-12 col-lg-6">
+        <!-- Prenotations chronology -->
+        <section id="booking-history" class="col-12 col-lg-6 py-2">
             <h2>Cronologia Prenotazioni</h2>
-            <?php var_dump($bills); ?>
+            <?php
+
+            if (!empty($bookingHistory)) {
+                foreach ($bookingHistory as $booking) {
+                    echo "<div class='booking-entry'>";
+                    echo "<p><strong>Date of order:</strong> " . htmlspecialchars($booking->created_at) . "</p>";
+                    echo "<p><strong>Send Adress:</strong> " . htmlspecialchars($booking->adress) . "</p>";
+                    echo "<p><strong>Name:</strong> " . htmlspecialchars($booking->customer) . "</p>";
+                    echo "<p><strong>Email:</strong> " . htmlspecialchars($booking->email) . "</p>";
+
+                    echo "<p><strong>List of the order:</strong></p>";
+                    $orderDetails = json_decode($booking->orderList, true);
+
+                    // Verifica che la decodifica sia andata a buon fine
+                    if ($orderDetails !== null) {
+                        echo "<ul>";
+                        // Ora puoi accedere ai dettagli dell'ordine come array associativo
+                        echo "<li>" . htmlspecialchars($orderDetails['movieTitle']) . " - " . htmlspecialchars($orderDetails['hallName']) . " - " . htmlspecialchars($orderDetails['showDetails']) . " - Seats: " . htmlspecialchars($orderDetails['seats']) . "</li>";
+                        echo "</ul>";
+                    } else {
+                        echo "Errore nel decodificare i dettagli dell'ordine.";
+                    }
+
+                    echo "</div><hr>";
+                }
+            } else {
+                echo "<p>You don't still order nothing</p>";
+            }
+            ?>
         </section>
     </div>
 </main>
