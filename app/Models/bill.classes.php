dac674e090f313ee4de53280923abdc812a793c3
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-03-22 11:46:20 +0100

    update display in checkout, with preparation of form, update bill model class

--- app/Models/bill.classes.php
+++ app/Models/bill.classes.php
@@ -4,29 +4,24 @@ class Bill {
 
     public $id;
     public $customer;
-    public $company;
     public $adresse;
-    public $city;
-    public $country;
-    public $zip;
     public $email;
-    public $phone;
     public $order_notes;
-    public $user_id;
     public $total;
-    public $productList;
+    public $orderList;
+    public $user_id;
     public $deliveryStatus;
 
 
-    public function __construct($id, $customer, $adresse, $email, $order_notes, $user_id, $total, $productList, $deliveryStatus) {
+    public function __construct($id, $customer, $adresse, $email, $order_notes, $total, $orderList, $user_id, $deliveryStatus) {
         $this->id = $id;
         $this->customer = $customer;
         $this->adresse = $adresse;
         $this->email = $email;
         $this->order_notes = $order_notes;
-        $this->user_id = $user_id;
         $this->total = $total;
-        $this->productList = $productList;
+        $this->orderList = $orderList;
+        $this->user_id = $user_id;
         $this->deliveryStatus = $deliveryStatus;
     }
 
--- resources/Views/checkout.php
+++ resources/Views/checkout.php
@@ -4,36 +4,95 @@
 if (!isset($_SESSION['userId'])) {
 
     echo "<script>window.location.href='http://" . $_SERVER['SERVER_NAME'] . "/BackendTemplateCinema/resources/Views/'</script>";
+}  else {
+    $userId = $_SESSION['userId'];
+    $cartItems = $cartController->getCartProUser($userId);
+    $user = $userController->getSingleUser($userId);
+
+    if (isset($_POST['procedePay'])) {
+        $customer = $_POST['fullname'];
+        $adress = $_POST['adress'];
+        $email = $_POST['email'];
+        $order_notes = $_POST['order_notes'];
+        $total = $_POST['totalPrice'];
+        $finalOrder = $_POST['orderList'];
+
+    }
+
 }
 
-if (empty($_GET['seats'])) {
-    echo "<script>window.location.href='http://" . $_SERVER['SERVER_NAME'] . "/BackendTemplateCinema/resources/Views/bookNow.php'</script>";
-    echo "<script>alert('Select a seat')</script>";
-    exit;
-}
+
 ?>
 
   <main class="container mt-5 mb-5 text-black bg-white">
+    <div class="row"><h1 class="text-success d-flex justify-content-center">Checkout</h1></div>
+    <form method="post" action="">
     <div class="row">
-        <div id="userInfo" class="col-12 col-md-6">
-            <h2>User Information</h2>
-            <p><strong>Name:</strong> John Doe</p>
-            <p><strong>Email:</strong> john@doe.com</p>
-            <p><strong>Adress:</strong> Street 65</p>
-            <p><strong>City:</strong> London</p>
+        <!-- Order Infos -->
+        <div class="col-12 col-md-6">
+            <?php foreach ($cartItems as $item) : ?>
+                <div id="showDetails" class="col-12">
+                    <h2>Show Details</h2>
+                    <p><strong>Movie:</strong> <?php echo htmlspecialchars($movieTitle = $movieController->getMovieById($item->movie_id)->title)?></p>
+                    <p><strong>Hall:</strong> <?php echo htmlspecialchars($hallName = $hallController->getHallById($item->hall_id)->name)?></p>
+                    <p><strong>Date and Time:</strong> <?php echo htmlspecialchars($showDate = $showController->getShowById($item->show_time_id)->showDate)?>, <?php echo htmlspecialchars($showTime = $showController->getShowById($item->show_time_id)->showTime)?></p>
+                    <p><strong>Seats:</strong>
+                        <?php
+                            $seatIdsArray = explode(',', $item->seat_ids);
+                            $seatDetails = "";
+                            $price = 0;
+                            foreach ($seatIdsArray as $seatId) {
+                                $singleSeat = $showSeatsController->getSeatPerId($seatId);
+                                $price += $singleSeat->price;
+                                $seatDetail = htmlspecialchars($singleSeat->seatNumber) . htmlspecialchars($singleSeat->row) . ", ";
+                                echo $seatDetail;
+                                $seatDetails .= $seatDetail;
+                            }
+                        ?>
+                    </p>
+                    <p><strong>Price:</strong> <?php echo htmlspecialchars($price); ?> €</p>
+                    <?php $totalPrice = ($totalPrice ?? 0) + $price; ?>
+                    <input type="hidden" name="totalPrice" value="<?php echo htmlspecialchars($totalPrice); ?>">
+                    <?php
+                        $orderDetails = [
+                            'movieTitle' => $movieTitle,
+                            'hallName' => $hallName,
+                            'showDetails' => $showDate . ", " . $showTime,
+                            'seats' => $seatDetails,
+                        ];
+                        $orderList = json_encode($orderDetails);
+                    ?>
+                    <input type="hidden" name="orderList" value='<?php echo htmlspecialchars($orderList); ?>'>
+                    <p><strong>Order:</strong> <?php echo htmlspecialchars($orderList)?></p>
+                </div>
+            <?php endforeach; ?>
         </div>
-        <div id="showDetails" class="col-12 col-md-6">
-            <h2>Show Details</h2>
-            <p><strong>Movie:</strong> Frozen</p>
-            <p><strong>Date and Time:</strong> 23 April, 13:00</p>
-            <p><strong>Hall:</strong> Jupiter</p>
-            <p><strong>Seats:</strong> 3A</p>
-            <p><strong>Total Price:</strong> 30 €</p>
+        
+        <!-- User Infos -->
+        <div id="userDetails" class="col-12 col-md-6">
+            <h2>User Details</h2>
+            <fieldset>
+                <div class="form-group pb-1">
+                    <input name="fullname" value="<?php echo htmlspecialchars($user->fullname); ?>" class="form-control" placeholder="Name" type="text" required>
+                </div>
+                <div class="form-group pb-1">
+                    <input name="email" value="<?php echo htmlspecialchars($user->email); ?>" class="form-control" placeholder="Email Address" type="email" required>
+                </div>
+                <div class="form-group pb-1">
+                    <input name="adress" type="text" class="form-control" placeholder="Adress" required>
+                </div>
+                <div class="form-group pb-1">
+                    <textarea name="order_notes" class="form-control" placeholder="Order Notes"></textarea>
+                </div>
+            </fieldset>
         </div>
+
         <div class="text-center my-4">
-            <button class="btn btn-success">Confirm and Pay</button>
+            <button type="submit" name="procedePay" class="btn btn-success">Confirm and Pay</button>
         </div>
-    </div>
+        </div>
+    </form>
+
 </main>
 
 <!-- Footer -->
