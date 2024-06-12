24c6327145c0839cebeecd747ee1a812bb1d6e1a
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-06-12 18:37:09 +0200

    add method for to fetch the shows and joined with movies halls and show_seats tables, for to display this datas

--- app/Controllers/ShowController.php
+++ app/Controllers/ShowController.php
@@ -15,11 +15,17 @@ class ShowController {
         return $this->showDAO->getShowsByMovieAndHall($movieId, $hallId);
     }
 
+    
     public function getShowById($id) {
         $showData = $this->showDAO->getShowById($id);
         if ($showData) {
             return new Show($showData->id, $showData->show_time, $showData->show_date, $showData->movie_id, $showData->hall_id);
         }
+        
         return null;
+    }
+
+    public function getAllShowWithMovieHallAndSeats() {
+        return $this->showDAO->getAllShowWithMovieHallAndSeats();
     }
 }
